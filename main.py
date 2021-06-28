1c4e7d32eb323893f1f49b11cbbeca3e8a1fcccd
ifAegis <70290676+ifAegis@users.noreply.github.com>
2021-06-27 22:28:29 -0400

    Update main.py
    Moved a note

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from resources import binary
 def main():
     target = check_value("Input ASCII : ")  # Get input from user
     output = ""
-    for x in target:
+    for x in target:  # Loop over target
         if x in binary:  # Check if value from target is in the binary dictionary
-            binary_dict = binary.get(x)  # Loop over target
+            binary_dict = binary.get(x)
             output += binary_dict + " "  # Append loop to output
         else:
             print("Error, please enter only non-special characters.")
